01096679858a7721fa6609ca87e98d91dbdb1bcc
ruizhong <raobystorm@gmail.com>
2017-08-17 16:51:13 +0900

    refactor & add test

--- train.py
+++ train.py
@@ -19,35 +19,35 @@ data_file = '/home/centos/audio-recognition/AudioSet/data.1502880153'
 eval_data_file = '/home/centos/audio-recognition/AudioSet/eval_data.dat'
 
 def random_sample(data_batch):
-  data_list = []
-  label_list = []
-  random.seed(int(time.time()))
-  for data in data_batch:
-    start_idx = random.randint(0, len(data[0]) - random_sample_size)
-    sample = data[0][start_idx : start_idx + random_sample_size]
-    data_list.append(np.reshape(sample, [n_input]))
-    label_list.append(np.reshape(data[1], [n_classes]))
-  return data_list, label_list
+    data_list = []
+    label_list = []
+    random.seed(int(time.time()))
+    for data in data_batch:
+        start_idx = random.randint(0, len(data[0]) - random_sample_size)
+        sample = data[0][start_idx : start_idx + random_sample_size]
+        data_list.append(np.reshape(sample, [n_input]))
+        label_list.append(np.reshape(data[1], [n_classes]))
+    return data_list, label_list
 
 def get_batch(data, batch_size, iteration):
-  start_of_batch = (iteration * batch_size) % len(data)
-  end_of_batch = (iteration * batch_size + batch_size) % len(data)
-  if start_of_batch < end_of_batch:
-    return data[start_of_batch:end_of_batch]
-  else:
-    start_of_batch = (start_of_batch + 1) % len(data)
-    end_of_batch = (end_of_batch + 1) % len(data)
-    data_batch = np.vstack((data[start_of_batch:],data[:end_of_batch]))
-    return data_batch
+    start_of_batch = (iteration * batch_size) % len(data)
+    end_of_batch = (iteration * batch_size + batch_size) % len(data)
+    if start_of_batch < end_of_batch:
+        return data[start_of_batch:end_of_batch]
+    else:
+        start_of_batch = (start_of_batch + 1) % len(data)
+        end_of_batch = (end_of_batch + 1) % len(data)
+        data_batch = np.vstack((data[start_of_batch:],data[:end_of_batch]))
+        return data_batch
 
 def load_data(file):
-  with open(file, 'rb') as fp:
-    data_block = pickle.load(fp)
-    for data in data_block:
-      data[0] = data[0].astype(np.float32)
-      data[1] = np.asarray(data[1])
-      data[1] = data[1].astype(np.float32)
-    return data_block
+    with open(file, 'rb') as fp:
+        data_block = pickle.load(fp)
+        for data in data_block:
+            data[0] = data[0].astype(np.float32)
+            data[1] = np.asarray(data[1])
+            data[1] = data[1].astype(np.float32)
+        return data_block
 
 sess = tf.InteractiveSession()
 
@@ -55,18 +55,18 @@ x = tf.placeholder(tf.float32, shape=[None, n_input])
 y_ = tf.placeholder(tf.float32, shape=[None, n_classes])
 
 def weight_varible(shape):
-  initial = tf.truncated_normal(shape, stddev=0.1)
-  return tf.Variable(initial)
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
 
 def bias_variable(shape):
-  initial = tf.constant(0.1, shape=shape)
-  return tf.Variable(initial)
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
 
 def conv2d(x, W):
-  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
 def max_pool(x, k):
-  return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1], padding='SAME')
+    return tf.nn.max_pool(x, ksize=[1, k, k, 1], strides=[1, k, k, 1], padding='SAME')
 
 # Reshape input
 x_image = tf.reshape(x, [-1, 128, 64, 1])
@@ -139,24 +139,25 @@ correct_prediction = tf.equal(tf.arg_max(y_conv, 1), tf.arg_max(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 with tf.Session() as sess:
-  data_ = load_data(data_file)
-  test_data = load_data(eval_data_file)
+    data_ = load_data(data_file)
+    test_data = load_data(eval_data_file)
 
-  if isLoad:
-    saver.restore(sess, model_save_path)
-  else:
-    sess.run(tf.global_variables_initializer())
-  for i in range(max_iter):
-    train_batch = random_sample(get_batch(data_, batch_size, i))
-    if i % 800 == 0:
-      train_accuacy = accuracy.eval(feed_dict={x: train_batch[0], y_: train_batch[1], keep_prob_1: 1.0})
-      print("step %d, training accuracy %g"%(i, train_accuacy))
-    train_step.run(feed_dict={x: train_batch[0], y_: train_batch[1], keep_prob_1: 0.5})
-    if i % 5000 == 0:
-      test_batch = random_sample(test_data)
-      print('test accuracy %g' % accuracy.eval(feed_dict={x: test_batch[0], y_: test_batch[1], keep_prob_1: 1.0}))
-      print('Model saved in %s' % saver.save(sess, model_save_path))
+    if isLoad:
+        saver.restore(sess, model_save_path)
+    else:
+        sess.run(tf.global_variables_initializer())
 
-  test_batch = random_sample(test_data)
-  print('test accuracy %g' % accuracy.eval(feed_dict={x: test_batch[0], y_: test_batch[1], keep_prob_1: 1.0}))
-  print('Model saved in %s' % saver.save(sess, model_save_path))
+    for i in range(max_iter):
+        train_batch = random_sample(get_batch(data_, batch_size, i))
+        if i % 800 == 0:
+            train_accuacy = accuracy.eval(feed_dict={x: train_batch[0], y_: train_batch[1], keep_prob_1: 1.0})
+            print("step %d, training accuracy %g"%(i, train_accuacy))
+        train_step.run(feed_dict={x: train_batch[0], y_: train_batch[1], keep_prob_1: 0.5})
+        if i % 5000 == 0:
+            test_batch = random_sample(test_data)
+            print('test accuracy %g' % accuracy.eval(feed_dict={x: test_batch[0], y_: test_batch[1], keep_prob_1: 1.0}))
+            print('Model saved in %s' % saver.save(sess, model_save_path))
+
+    test_batch = random_sample(test_data)
+    print('test accuracy %g' % accuracy.eval(feed_dict={x: test_batch[0], y_: test_batch[1], keep_prob_1: 1.0}))
+    print('Model saved in %s' % saver.save(sess, model_save_path))
