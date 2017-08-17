fbc62008c9920911ef150ef2c24445754d3f24bf
ruizhong <raobystorm@gmail.com>
2017-08-17 17:18:13 +0900

    fix test

--- test_downloaded_files.py
+++ test_downloaded_files.py
@@ -2,15 +2,17 @@ import csv
 import os
 
 with open('/home/centos/audio-recognition/AudioSet/unbalanced_train_segments.csv', mode='r') as fp:
-    reader = csv.reader(source)
+    reader = csv.reader(fp)
     str_list = []
     for row in reader:
         str_list.append(row)
 
     for filename in os.listdir('/home/centos/audio-recognition/AudioSet/music'):
         filename = filename.rsplit('.', 1)[0]
+        print(filename)
         assert '/m/04rlf' in str(list(filter(lambda x: filename in x, str_list))[0])
 
     for filename in os.listdir('/home/centos/audio-recognition/AudioSet/nonmusic'):
         filename = filename.rsplit('.', 1)[0]
+        print(filename)
         assert '/m/04rlf' not in str(list(filter(lambda x: filename in x, str_list))[0])
