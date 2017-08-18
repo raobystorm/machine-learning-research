e6bcb26e3708f587ff9e4e4c2962056362791117
ruizhong <raobystorm@gmail.com>
2017-08-18 12:04:43 +0900

    eval data preproces

--- preprocess.py
+++ preprocess.py
@@ -8,12 +8,12 @@ class AudioSetPreprocess(object):
     def __init__(self):
         self.random_sample_size = 128
         self.base_url = '/home/centos/audio-recognition/AudioSet'
-        self.music_files_path = self.base_url + '/music'
+        #self.music_files_path = self.base_url + '/music'
+        self.music_files_path = self.base_url + '/processed/music'
         self.music_files_limit = 6000
-        self.nonmusic_files_path = self.base_url + '/nonmusic'
+        #self.nonmusic_files_path = self.base_url + '/nonmusic'
+        self.nonmusic_files_path = self.base_url + '/processed/nonmusic'
         self.nonmusic_files_limit = 14000
-        self.processed_music_files_path = self.base_url + '/processed/music'
-        self.processed_nonmusic_files_path = self.base_url + '/processed/nonmusic'
         self.eval_music_files_path = self.base_url + '/eval_music'
         self.eval_nonmusic_files_path = self.base_url + '/eval_nonmusic'
 
@@ -61,8 +61,8 @@ class AudioSetPreprocess(object):
         librosa.cache.clear()
         processed_list = self.preprocess()
         print(len(processed_list))
-        with open(self.base_url + '/data.' + datetime.now().strftime('%s'), 'wb') as fp:
-            #with open(base_url + '/eval_data.dat', 'wb') as fp:
+        #with open(self.base_url + '/data.' + datetime.now().strftime('%s'), 'wb') as fp:
+        with open(base_url + '/eval_data.dat', 'wb') as fp:
             pickle.dump(processed_list, fp)
             librosa.cache.clear()
 
