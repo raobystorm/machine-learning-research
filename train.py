cb7fd599fc9da70b6b31a3c39a3e230b8d1d22c7
ruizhong <raobystorm@gmail.com>
2017-08-21 11:32:01 +0900

    change batch size

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ import random
 n_input = 128 * 64
 n_classes = 2
 max_iter = 300000
-batch_size = 32
+batch_size = 40
 random_sample_size = 128
 isLoad = False
 
@@ -151,7 +151,7 @@ with tf.Session() as sess:
         if i % 800 == 0:
             train_accuacy = accuracy.eval(feed_dict={x: train_batch[0], y_: train_batch[1], keep_prob_1: 1.0})
             print("step %d, training accuracy %g"%(i, train_accuacy))
-        if i > 6400:
+        #if i > 6400:
             #print('step: %d' % i)
             #print('batch size: %d' % len(train_batch))
             #print('train_batch[0] size: %d' % len(train_batch[0]))
