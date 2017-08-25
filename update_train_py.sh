07edd90a24b26270ae6944792121467ecf3102b8
ruizhong <raobystorm@gmail.com>
2017-08-25 14:08:19 +0900

    no message

--- update_train_py.sh
+++ update_train_py.sh
@@ -1,4 +1,4 @@
 #!bin bash
-git checkout train.py download.py preprocess.py
+git checkout train.py download.py preprocess.py test_model.py
 git pull
-sed -i 's/centos/raoby/g' train.py download.py preprocess.py
+sed -i 's/centos/raoby/g' train.py download.py preprocess.py test_model.py
