626b774d1bba8d6c8bbc184179f5a774ae698f81
ruizhong <raobystorm@gmail.com>
2017-09-07 12:33:54 +0900

    no message

--- prepare_test.py
+++ prepare_test.py
@@ -6,12 +6,15 @@ import librosa
 folder = '/home/centos/audio-recognition/test/test_medium/'
 csv_file = folder + 'test.csv'
 
+random_sample_size = 256
+
 proc_list = []
 
 with open(csv_file, 'r') as cf:
     reader = csv.reader(cf)
     for row in reader:
         filename = row[0] + '.mp4'
+        print('start process %s' % filename)
         is_music = int(row[1])
         y, sr = librosa.load(folder + filename, sr=44100)
         if len(y) is not 0:
@@ -19,10 +22,11 @@ with open(csv_file, 'r') as cf:
             mfcc = mfcc.transpose()
             # For some samples the length is insufficient, just ignore them
             if len(mfcc) >= random_sample_size:
+                print('file size of %s is %g * 64' % (filename, len(mfcc)))
                 if is_music == 1:
-                    proc_list.append([mfcc, [1., 0.]])
-                else:
                     proc_list.append([mfcc, [0., 1.]])
+                else:
+                    proc_list.append([mfcc, [1., 0.]])
 
 print(len(proc_list))
 
