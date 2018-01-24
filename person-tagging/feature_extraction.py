a963e742d81d846c40cb5b73cdf9f2e1db21cbf1
ruizhong <raobystorm@gmail.com>
2018-01-24 17:10:56 +0900

    split into refer set & test set

--- person-tagging/feature_extraction.py
+++ person-tagging/feature_extraction.py
@@ -2,9 +2,11 @@
 
 import os
 import sys
+import cPickle
 
 caffe_root = './'
 sys.path.insert(0, caffe_root + 'python')
+gallery_ratio = 0.7
 
 import caffe
 
@@ -18,20 +20,30 @@ transformer.set_transpose('data', (2, 0, 1))
 net.blobs['data'].reshape(1, 3, 227, 227)
 
 base_folder = '/home/centos/mitene-pre_experiment/results'
-json_str = '['
+test_set = []
+reference_set = []
+
 for sub_folder in os.listdir(base_folder):
     extracted_features = []
+    img_count = 0
+    total_count = len(os.listdir(base_folder))
+    reference_count = total_count * gallery_ratio
     for img in os.listdir(base_folder + '/' + sub_folder):
         if os.path.splitext(img)[1] != '.jpg':
             continue
-        image = caffe.io.load_image(base_folder + '/' + sub_folder + '/' + img)
+        image = caffe.io.load_image(base_folder + '/' + sub_folder + '/images/' + img)
         transformed_image = transformer.preprocess('data', image)
         net.blobs['data'].data[...] = transformed_image
         output = net.forward()
-        json_str += '{"' + img + '" : ' + str(output['fc7'].tolist()[0]) + '},\n'
-        print "processed: " + img
+        if img_count < reference_count:
+            reference_set << output
+            print 'processed: reference image: ' + img + ', reference count: ' + str(img_count) + '/' + str(reference_count)
+        else:
+            test_set << output
+            print 'processed: test image: ' + img + ', test count: ' + str(img_count - reference_count) + '/' + str(total_count)
+        img_count += 1
 
-    json_str = json_str[:-1][:-1]
-    json_str += ']'
-    with open(base_folder + '/' + sub_folder + '/data_' + sub_folder + '.dat', 'w') as f:
-        f.write(json_str)
+    with open(base_folder + '/' + sub_folder + '/data_' + sub_folder + '_reference.dat', 'w') as f:
+        cPickle.dump(reference_set, f)
+    with open(base_folder + '/' + sub_folder + '/data_' + sub_folder + '_test.dat', 'w') as f:
+        cPickle.dump(test_set, f)
