0115db79de8c96bd35a2cebdd811aaab8927806c
ruizhong <raobystorm@gmail.com>
2018-01-24 20:13:22 +0900

    clustered extracted features

---
+++ person-tagging/erclustering.py
@@ -0,0 +1,38 @@
+# Python 2.7
+
+import os
+import cPickle
+import erclustering
+
+base_folder = '/home/centos/mitene-pre_experiment/results'
+ref_set = []
+ref_filename_list = []
+test_set = []
+test_filename_list = []
+
+F = 4096
+gallery_size = 50
+K = 200
+
+for sub_folder in os.listdir(base_folder):
+    with open(base_folder + '/' + sub_folder + '/data_' + sub_folder + '_reference.dat', 'rb') as f:
+        ref_raw = cPickle.load(f)
+
+    with open(base_folder + '/' + sub_folder + '/data_' + sub_folder + '_test.dat', 'rb') as f:
+        test_raw = cPickle.load(f)
+
+    print 'Reference set and test set loaded! Start clustering for ' + sub_folder + ' data set.'
+
+    N = len(ref_raw)
+    for array, img_name in ref_raw:
+        ref_set.append(array)
+        ref_filename_list.append(img_name)
+    ref_set = map(list, zip(*ref_set))
+
+    G = len(test_raw)
+    for array, img_name in test_raw:
+        test_set.append(array)
+        test_filename_list.append(img_name)
+    test_set = map(list, zip(*test_set))
+
+    result = erclustering.Rank1Count(test_set, ref_set, G, N, F, K, gallery_size)
--- person-tagging/feature_extraction.py
+++ person-tagging/feature_extraction.py
@@ -24,26 +24,28 @@ test_set = []
 reference_set = []
 
 for sub_folder in os.listdir(base_folder):
-    extracted_features = []
     img_count = 0
-    total_count = len(os.listdir(base_folder))
-    reference_count = total_count * gallery_ratio
-    for img in os.listdir(base_folder + '/' + sub_folder):
+    img_folder = base_folder + '/' + sub_folder + '/images'
+    total_count = len(os.listdir(img_folder))
+    reference_count = int(total_count * gallery_ratio)
+    for img in os.listdir(img_folder):
         if os.path.splitext(img)[1] != '.jpg':
             continue
-        image = caffe.io.load_image(base_folder + '/' + sub_folder + '/images/' + img)
+        image = caffe.io.load_image(img_folder + '/' + img)
         transformed_image = transformer.preprocess('data', image)
         net.blobs['data'].data[...] = transformed_image
         output = net.forward()
+        output = output['fc7'].tolist()
+        output.append(img)
         if img_count < reference_count:
-            reference_set << output
+            reference_set.append(output)
             print 'processed: reference image: ' + img + ', reference count: ' + str(img_count) + '/' + str(reference_count)
         else:
-            test_set << output
-            print 'processed: test image: ' + img + ', test count: ' + str(img_count - reference_count) + '/' + str(total_count)
+            test_set.append(output)
+            print 'processed: test image: ' + img + ', test count: ' + str(img_count - reference_count) + '/' + str(total_count - reference_count)
         img_count += 1
 
-    with open(base_folder + '/' + sub_folder + '/data_' + sub_folder + '_reference.dat', 'w') as f:
+    with open(base_folder + '/' + sub_folder + '/data_' + sub_folder + '_reference.dat', 'wb') as f:
         cPickle.dump(reference_set, f)
-    with open(base_folder + '/' + sub_folder + '/data_' + sub_folder + '_test.dat', 'w') as f:
+    with open(base_folder + '/' + sub_folder + '/data_' + sub_folder + '_test.dat', 'wb') as f:
         cPickle.dump(test_set, f)
