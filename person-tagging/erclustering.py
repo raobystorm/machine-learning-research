622c7f362ec74f4ad3585130e8fb38f155d091b1
ruizhong <raobystorm@gmail.com>
2018-01-29 17:18:42 +0900

    1, add nonzerorization to feature extraction for shrinking float value for Rank1Count 2, Write result into text file 3, reverted preprocess part for images, no need

--- person-tagging/erclustering.py
+++ person-tagging/erclustering.py
@@ -2,6 +2,7 @@
 
 import os
 import cPickle
+import random
 import erclustering
 
 base_folder = '/home/centos/mitene-pre_experiment/results'
@@ -14,25 +15,39 @@ F = 4096
 gallery_size = 50
 K = 200
 
+def nonzerorization(mat):
+    for i in range(len(mat)):
+        for j in range(len(mat[i])):
+            if mat[i][j] == 0.0:
+                mat[i][j] = random.random() * 1e-5
+
+def get_data_from_file(f):
+    raw = cPickle.load(f)
+    count = len(raw)
+    data_set = []
+    filename_list = []
+    for array, img_name in raw:
+        data_set.append(array)
+        filename_list.append(img_name)
+    data_set = map(list, zip(*data_set))
+    nonzerorization(data_set)
+    return data_set, filename_list, count
+
 for sub_folder in os.listdir(base_folder):
     with open(base_folder + '/' + sub_folder + '/data_' + sub_folder + '_reference.dat', 'rb') as f:
-        ref_raw = cPickle.load(f)
+        ref_raw, ref_filename_list, G = get_data_from_file(f)
 
     with open(base_folder + '/' + sub_folder + '/data_' + sub_folder + '_test.dat', 'rb') as f:
-        test_raw = cPickle.load(f)
+        test_raw, test_filename_list, N = get_data_from_file(f)
 
     print 'Reference set and test set loaded! Start clustering for ' + sub_folder + ' data set.'
+    result = erclustering.Rank1Count(test_raw, ref_raw, G, N, F, K, gallery_size)
 
-    G = len(ref_raw)
-    for array, img_name in ref_raw:
-        ref_set.append(array)
-        ref_filename_list.append(img_name)
-    ref_set = map(list, zip(*ref_set))
-    
-    N = len(test_raw)
-    for array, img_name in test_raw:
-        test_set.append(array)
-        test_filename_list.append(img_name)
-    test_set = map(list, zip(*test_set))
-
-    result = erclustering.Rank1Count(test_set, ref_set, G, N, F, K, gallery_size)
+    with open(base_folder + '/' + sub_folder + '/result_' + sub_folder + '.dat', 'w') as f:
+        for filename in test_filename_list:
+            f.write(filename)
+            f.write(',')
+        f.write('\n')
+        for res in result:
+            f.write(str(res).replace('[', '').replace(']', ''))
+            f.write('\n')
--- person-tagging/feature_extraction.py
+++ person-tagging/feature_extraction.py
@@ -3,33 +3,12 @@
 import os
 import sys
 import cPickle
-import numpy as np
 
 caffe_root = './'
 sys.path.insert(0, caffe_root + 'python')
 gallery_ratio = 0.95
-image_size = (227, 227)
 
 import caffe
-
-class Image(object):
-    def __init__(self, image, name):
-        self.image = image
-        self.name = name
-
-
-def preprocess_image(image, mean, std):
-    image.image -= mean
-    image.image /= std
-    return image
-
-def feature_extract(image_name, net):
-    transformed_image = transformer.preprocess('data', image_name.image)
-    net.blobs['data'].data[...] = transformed_image
-    output = net.forward()
-    output = output['fc7'].tolist()
-    return [output, image_name.name]
-
 
 caffe.set_mode_gpu()
 model_def = caffe_root + 'models/mitene_test/alexnet_extraction.prototxt'
@@ -41,12 +20,9 @@ transformer.set_transpose('data', (2, 0, 1))
 net.blobs['data'].reshape(1, 3, 227, 227)
 
 base_folder = '/home/centos/mitene-pre_experiment/results'
-ref_images_name = []
-test_images_name = []
-ref_set = []
 test_set = []
+reference_set = []
 
-# Read image data and prepare for preprocess
 for sub_folder in os.listdir(base_folder):
     img_count = 1
     img_folder = base_folder + '/' + sub_folder + '/images'
@@ -56,36 +32,20 @@ for sub_folder in os.listdir(base_folder):
         if os.path.splitext(img)[1] != '.jpg':
             continue
         image = caffe.io.load_image(img_folder + '/' + img)
-        image = caffe.io.resize_image(image, image_size)
+        transformed_image = transformer.preprocess('data', image)
+        net.blobs['data'].data[...] = transformed_image
+        output = net.forward()
+        output = output['fc7'].tolist()
+        output.append(img)
         if img_count <= reference_count:
-            ref_images_name.append(Image(image=image, name=img))
+            reference_set.append(output)
+            print 'processed: reference image: ' + img + ', reference count: ' + str(img_count) + '/' + str(reference_count)
         else:
-            test_images_name.append(Image(image=image, name=img))
+            test_set.append(output)
+            print 'processed: test image: ' + img + ', test count: ' + str(img_count - reference_count) + '/' + str(total_count - reference_count)
         img_count += 1
 
-    # Preprocess for image, calc mean and deviation
-    print 'Start calc image mean and deviation for reference images.' + sub_folder
-    ref_image = [ data.image for data in ref_images_name ]
-    ref_mean = np.mean(ref_image, axis=0)
-    ref_std = np.std(ref_image, axis=0)
-
-    ref_images_name = [ preprocess_image(image, ref_mean, ref_std) for image in ref_images_name ]
-
-    print 'Start calc image mean and deviation for test images.' + sub_folder
-    test_image = [ data.image for data in test_images_name ]
-    test_mean = np.mean(test_image, axis=0)
-    test_std = np.std(test_image, axis=0)
-
-    test_images_name = [ preprocess_image(image, test_mean, test_std) for image in test_images_name ]
-
-    print 'Start feature extraction for reference images.' + sub_folder
-    ref_images_name = [ feature_extract(image_name, net) for image_name in ref_images_name ]
-
-    print 'Start feature extraction for test images.' + sub_folder
-    test_images_name = [ feature_extract(image_name, net) for image_name in test_images_name ]
-
     with open(base_folder + '/' + sub_folder + '/data_' + sub_folder + '_reference.dat', 'wb') as f:
-        cPickle.dump(ref_images_name, f)
-
+        cPickle.dump(reference_set, f)
     with open(base_folder + '/' + sub_folder + '/data_' + sub_folder + '_test.dat', 'wb') as f:
-        cPickle.dump(test_images_name, f)
+        cPickle.dump(test_set, f)
